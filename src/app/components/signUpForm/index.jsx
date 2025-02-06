4a1ae9bce682af0294298ee484ec40847c02e723
Exclusiveideas <muftaudeenoluwatosin@gmail.com>
2025-02-06 22:26:41 +0100

    fixed waitlist signup error

--- src/app/components/signUpForm/index.jsx
+++ src/app/components/signUpForm/index.jsx
@@ -14,12 +14,13 @@ const SignupForm = () => {
   const [loading, setLoading] = useState(false);
   const [message, setMessage] = useState("");
 
-  const onSubmit = async (data) => {
+  const onSubmit = async (e) => {
+    e.preventDefault()
     setLoading(true);
     setMessage("");
 
     const payload = {
-      email: data.email,
+      email: e.target[0].value,
       waitlist_id: 24919, // Your Waitlist ID
       referral_link: window.location.href, // Capture current page URL
     };
@@ -38,6 +39,8 @@ const SignupForm = () => {
 
       const result = await response.json();
 
+      console.log('response: ', response)
+
       if (response.ok) {
         setMessage(`Success! Your referral link: ${result.referral_link}`);
         reset(); // Reset form after success
@@ -54,7 +57,7 @@ const SignupForm = () => {
   return (
     <div className="heroWaitlist_wrapper">
       <p className="joinText">Join 19 other Editors on the Waitlist !</p>
-      <form className="waitlistForm" onSubmit={handleSubmit(onSubmit)}>
+      <form className="waitlistForm" onSubmit={(e) => handleSubmit(onSubmit(e))}>
         <input
           name="email"
           type="email"
